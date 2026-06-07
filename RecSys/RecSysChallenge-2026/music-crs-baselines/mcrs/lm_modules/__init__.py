08695a0f461f3a7e8762cb5ae7489e13765e3039
zhenhao.jiang <zhenhao.jiang@amh-group.com>
2026-06-07 16:27:30 +0800

    update 260607

--- RecSys/RecSysChallenge-2026/music-crs-baselines/mcrs/lm_modules/__init__.py
+++ RecSys/RecSysChallenge-2026/music-crs-baselines/mcrs/lm_modules/__init__.py
@@ -1,12 +1,7 @@
-# from .llama import LLAMA_MODEL
-# from .qwen import QWEN_MODEL
-from .lm import LLM_MODEL
+from .llama import LLAMA_MODEL
 
 def load_lm_module(lm_type, device, attn_implementation, dtype):
-    return LLM_MODEL(model_name=lm_type, device=device, attn_implementation=attn_implementation, dtype=dtype)
-    # if lm_type == "Qwen/Qwen3-4B":
-    #     return QWEN_MODEL(model_name=lm_type, device=device, attn_implementation=attn_implementation, dtype=dtype)
-    # elif lm_type == "meta-llama/Llama-3.2-1B-Instruct":
-    #     return LLAMA_MODEL(model_name=lm_type, device=device, attn_implementation=attn_implementation, dtype=dtype)
-    # else:
-    #     raise ValueError(f"Unsupported LM type: {lm_type}")
+    if lm_type == "meta-llama/Llama-3.2-1B-Instruct":
+        return LLAMA_MODEL(model_name=lm_type, device=device, attn_implementation=attn_implementation, dtype=dtype)
+    else:
+        raise ValueError(f"Unsupported LM type: {lm_type}")
